387392e4906aefce9e0dc6c9b4f2d2e44709581e
Nasar <mohd_nasar@optum.com>
2025-08-06 20:44:25 +0530

    file added

---
+++ shell_script_docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#meta data: THis script will install docker on the ubuntu server and pull docker image and run that image.
+
+#Author Mohd Nasar
+
+sudo apt update -y
+
+sudo apt install docker.io -y 
+
+usermod -aG docker ubuntu
+
+docker --version
+
+docker pull mnasar12/rps
+
+docker run mnasar12/rps 
+
+docker ps
